0b7cc0a96e884326baade0e8548d8815e1c9317a
Daniel Glover <iamglover02@gmail.com>
2023-10-03 16:14:46 +0000

    homepage carousel completion

--- src/app/modules/homepage/homepage.component.ts
+++ src/app/modules/homepage/homepage.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class HomepageComponent {
 
+
+  // Carousel Implementation
+
+  carouselImages: string[] = ['image1', 'image2', 'image3']; // CSS classes for background images
+  currentIndex: number = 0;
+  currentImage: string = this.carouselImages[this.currentIndex];
+  interval: number = 3000; // Interval in milliseconds for carousel
+
+  ngOnInit(){
+    // Moving to next image after specified interval
+    setInterval(() => {
+      this.next();
+    }, this.interval);
+  }
+
+  next(){
+    this.currentIndex = (this.currentIndex + 1) % this.carouselImages.length;
+    this.currentImage = this.carouselImages[this.currentIndex];
+  }
+
 }
