e02bef7f50853f69e41f4622e8e55a1e098a970c
Daniel Glover <iamglover02@gmail.com>
2023-11-19 17:43:47 +0000

    image formats change

--- src/app/modules/homepage/pages/about-us/about-us.component.ts
+++ src/app/modules/homepage/pages/about-us/about-us.component.ts
@@ -10,14 +10,14 @@ export class AboutUsComponent {
 
 
   teamMembers: TeamMembers[] = [
-    {memberImg: '/assets/images/deborah.jpeg', memberName: 'Deborah McConway', memberRole: 'Co-Founder / CEO', iconContainerBg: 'rgba(69, 0, 255, 1)', 
+    {memberImg: '/assets/images/deborah.jpg', memberName: 'Deborah McConway', memberRole: 'Co-Founder / CEO', iconContainerBg: 'rgba(69, 0, 255, 1)', 
     iconContainer: [
       {background: 'rgba(69, 0, 255, 1)', icon: '/assets/svgs/icn-twitter-outline.svg', iconColor: 'invert(98%) sepia(100%) saturate(0%) hue-rotate(121deg) brightness(116%) contrast(100%);', iconLink: 'https://x.com'},
       {background: 'rgba(69, 0, 255, 1)', icon: '/assets/svgs/icn-linkedin-outline.svg', iconColor: 'invert(98%) sepia(100%) saturate(0%) hue-rotate(121deg) brightness(116%) contrast(100%);', iconLink: 'https://linkedin.com'},
       {background: 'rgba(69, 0, 255, 1)', icon: '/assets/svgs/icn-email-outline.svg', iconColor: 'invert(98%) sepia(100%) saturate(0%) hue-rotate(121deg) brightness(116%) contrast(100%);', iconLink: 'mailto:mojo0@gmail.com'}
     ]},
 
-    {memberImg: '/assets/images/bassam-haikal.jpeg', memberName: 'Bassam Haikal', memberRole: 'Co-Founder / Executive Chairman', iconContainerBg: 'rgba(69, 0, 255, 1)', 
+    {memberImg: '/assets/images/bassam-haikal.jpg', memberName: 'Bassam Haikal', memberRole: 'Co-Founder / Executive Chairman', iconContainerBg: 'rgba(69, 0, 255, 1)', 
     iconContainer: [
       {background: 'rgba(105, 224, 122, 1)', icon: '/assets/svgs/icn-twitter-outline.svg', iconColor: 'invert(10%) sepia(78%) saturate(5885%) hue-rotate(258deg) brightness(116%) contrast(145%);', iconLink: 'https://x.com'},
       {background: 'rgba(105, 224, 122, 1)', icon: '/assets/svgs/icn-linkedin-outline.svg', iconColor: 'invert(10%) sepia(78%) saturate(5885%) hue-rotate(258deg) brightness(116%) contrast(145%);', iconLink: 'https://linkedin.com'},
@@ -31,7 +31,7 @@ export class AboutUsComponent {
       {background: 'rgba(255, 140, 66, 1)', icon: '/assets/svgs/icn-email-outline.svg', iconColor: 'invert(100%) sepia(0%) saturate(7500%) hue-rotate(100deg) brightness(102%) contrast(101%);', iconLink: 'mailto:mojo0@gmail.com'}
     ]},
 
-    {memberImg: '/assets/images/saqib-nazir.jpeg', memberName: 'Saqib Nazir', memberRole: 'Co-Founder / Director', iconContainerBg: 'rgba(69, 0, 255, 1)', 
+    {memberImg: '/assets/images/saqib-nazir.jpg', memberName: 'Saqib Nazir', memberRole: 'Co-Founder / Director', iconContainerBg: 'rgba(69, 0, 255, 1)', 
     iconContainer: [
       {background: 'rgba(105, 224, 122, 1)', icon: '/assets/svgs/icn-twitter-outline.svg', iconColor: 'invert(10%) sepia(78%) saturate(5885%) hue-rotate(258deg) brightness(116%) contrast(145%);', iconLink: 'https://x.com'},
       {background: 'rgba(105, 224, 122, 1)', icon: '/assets/svgs/icn-linkedin-outline.svg', iconColor: 'invert(10%) sepia(78%) saturate(5885%) hue-rotate(258deg) brightness(116%) contrast(145%);', iconLink: 'https://linkedin.com'},
--- src/app/modules/homepage/pages/blog/blog.component.ts
+++ src/app/modules/homepage/pages/blog/blog.component.ts
@@ -49,8 +49,8 @@ export class BlogComponent {
 
 
   newscards: NewsCards[] = [
-    {newsImg: '/assets/images/news-img1.png', writerImg: '/assets/images/blog-writer.jpeg', writerName: 'L’Orient Today', newsDate: 'June 03, 2023', newsTitle: 'Digital wallets, a new way to bypass banks in Lebanon?', newsSubTitle: 'Since mid-May, a small revolution has been taking place in Lebanon’s digital wallet market, as the first steps towards synergy among...', newsLink: 'https://today.lorientlejour.com/article/1339336/digital-wallets-a-new-way-to-bypass-banks-in-lebanon.html'},
-    {newsImg: '/assets/images/news-img2.jpeg', writerImg: '/assets/images/blog-writer.jpeg', writerName: 'Lebanese  University', newsDate: 'Sept 14, 2023', newsTitle: 'Health Insurance Premiums for Students for Fall 2023', newsSubTitle: 'Students enrolled in any university in Lebanon, are required by law to be covered by health insurance.', newsLink: 'https://today.lorientlejour.com/article/1339336/digital-wallets-a-new-way-to-bypass-banks-in-lebanon.html'},
+    {newsImg: '/assets/images/news-img1.png', writerImg: '/assets/images/blog-writer.jpg', writerName: 'L’Orient Today', newsDate: 'June 03, 2023', newsTitle: 'Digital wallets, a new way to bypass banks in Lebanon?', newsSubTitle: 'Since mid-May, a small revolution has been taking place in Lebanon’s digital wallet market, as the first steps towards synergy among...', newsLink: 'https://today.lorientlejour.com/article/1339336/digital-wallets-a-new-way-to-bypass-banks-in-lebanon.html'},
+    {newsImg: '/assets/images/news-img2.jpg', writerImg: '/assets/images/blog-writer.jpg', writerName: 'Lebanese  University', newsDate: 'Sept 14, 2023', newsTitle: 'Health Insurance Premiums for Students for Fall 2023', newsSubTitle: 'Students enrolled in any university in Lebanon, are required by law to be covered by health insurance.', newsLink: 'https://today.lorientlejour.com/article/1339336/digital-wallets-a-new-way-to-bypass-banks-in-lebanon.html'},
     
   ]
   
