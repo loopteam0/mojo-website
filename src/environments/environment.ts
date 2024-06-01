263541d580594ac616ca11f656fe84c9e4fad9ee
Daniel Glover <iamglover02@gmail.com>
2024-06-01 01:03:24 +0000

    starting recaptcha implementation

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    // baseUrl: "https://mpampabackend.vercel.app"
+    // baseUrl: "http://localhost:2005"
+    recaptcha: {
+        siteKey: '6LdZCO4pAAAAABOnOjlJ0QP-tyOTg50VMTnwYdbF',
+    },
+
+}
